9acf2a04c67a6f19aa4c81ff157abc91c8a2ee85
ezylot <florian.schoeffl@gmail.com>
2018-04-29 07:13:08 +0200

    Rewrote coordinated for rocket

--- objects/rocket.js
+++ objects/rocket.js
@@ -1,30 +1,23 @@
 function createRocketNode(gl) {
-    let s = 0.3; //size of cube
     let cubeVertices = new Float32Array([
-        -s,-s,-s,   s,-s,-s,    s, s,-s,    -s, s,-s,
-        -s,-s, s,   s,-s, s,    s, s, s,    -s, s, s,
-        -s,-s,-s,   -s, s,-s,   -s, s, s,   -s,-s, s,
-        s,-s,-s,    s, s,-s,    s, s, s,    s,-s, s,
-        -s,-s,-s,   -s,-s, s,   s,-s, s,    s,-s,-s,
-        -s, s,-s,   -s, s, s,   s, s, s,    s, s,-s,
-    ]);
+        -1,-1,-1,   // 0 - DLB
+        -1,-1,1,    // 1 - DLF
+        1,-1,1,     // 2 - DRF
+        1,-1,-1,    // 3 - DRF
 
-    let cubeColors = new Float32Array([
-        0,1,1, 0,1,1, 0,1,1, 0,1,1,
-        1,0,1, 1,0,1, 1,0,1, 1,0,1,
-        1,0,0, 1,0,0, 1,0,0, 1,0,0,
-        0,0,1, 0,0,1, 0,0,1, 0,0,1,
-        1,1,0, 1,1,0, 1,1,0, 1,1,0,
-        0,1,0, 0,1,0, 0,1,0, 0,1,0
+        -1,1,-1,   // 4 - ULB
+        -1,1,1,    // 5 - ULF
+        1,1,1,     // 6 - URF
+        1,1,-1,    // 7 - URF
     ]);
 
     let cubeIndices =  new Float32Array([
-        0,1,2, 0,2,3,
-        4,5,6, 4,6,7,
-        8,9,10, 8,10,11,
-        12,13,14, 12,14,15,
-        16,17,18, 16,18,19,
-        20,21,22, 20,22,23
+        0,1,2,0,2,3,    //DOWN
+        4,5,6,4,6,7,    //UP
+        5,1,2,5,2,6,    //FACING FRONT
+        6,2,3,6,3,7,    //FACING SIDE
+        0,4,3,4,7,3,    //NON-FACING BACK
+        4,5,0,5,0,1,    //NON-FACING SIDE
     ]);
 
     let cubeVertexBuffer = gl.createBuffer();
@@ -32,8 +25,6 @@ function createRocketNode(gl) {
     gl.bufferData(gl.ARRAY_BUFFER, cubeVertices, gl.STATIC_DRAW);
 
     let cubeColorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cubeColorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, cubeColors, gl.STATIC_DRAW);
 
     let cubeIndexBuffer = gl.createBuffer ();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeIndexBuffer);
