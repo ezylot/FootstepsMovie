1a7cbbde1ff5c51eeb733f60ca8472dfd3a0f714
ezylot <florian.schoeffl@gmail.com>
2018-04-29 18:33:27 +0200

    Added tip of rocket

--- objects/rocket.js
+++ objects/rocket.js
@@ -1,14 +1,66 @@
 function createRocketNode(gl) {
-    let cubeVertices = new Float32Array([
-        -1,-1,-1,   // 0 - DLB
-        -1,-1,1,    // 1 - DLF
-        1,-1,1,     // 2 - DRF
-        1,-1,-1,    // 3 - DRF
 
-        -1,1,-1,   // 4 - ULB
-        -1,1,1,    // 5 - ULF
+    let rocketNode = new RootNode();
+
+    // Body of rocket
+    let bodyTransformationMatrix = mat4.create();
+    bodyTransformationMatrix = mat4.multiply(mat4.create(), bodyTransformationMatrix, glm.scale(1, 2, 1));
+    let bodyTransformationNode = new TransformationNode(bodyTransformationMatrix);
+    rocketNode.append(bodyTransformationNode);
+    bodyTransformationNode.append(createBody(gl));
+
+    // Tip of rocket
+    let tipTransformationMatrix = mat4.create();
+    tipTransformationMatrix = mat4.multiply(mat4.create(), tipTransformationMatrix, glm.translate(0, 1, 0));
+    let tipTransformationNode = new TransformationNode(tipTransformationMatrix);
+    rocketNode.append(tipTransformationNode);
+    tipTransformationNode.append(createTip(gl));
+
+    return rocketNode;
+}
+
+function createTip(gl) {
+    let pyramidVertices =  new Float32Array([
+        0,0,0,   // 0 - DLB
+        0,0,1,    // 1 - DLF
+        1,0,1,     // 2 - DRF
+        1,0,0,    // 3 - DRF
+        0.5,1,0.5,      // 4 tip
+    ]);
+
+    let pyramidIndices =  new Float32Array([
+        0,1,2,0,2,3,    // Bottom
+        4,1,2,          // Front
+        4,1,0,          // Left
+        4,0,3,          // Back
+        4,2,3,          // Right
+    ]);
+
+    let pyramidVertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, pyramidVertices, gl.STATIC_DRAW);
+
+    let cubeColorBuffer = gl.createBuffer();
+
+    let pyramidIndexBuffer = gl.createBuffer ();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, pyramidIndexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(pyramidIndices), gl.STATIC_DRAW);
+
+    return new IndexedVerticesRenderNode(pyramidVertexBuffer, pyramidIndexBuffer, pyramidIndices.length, cubeColorBuffer, 1);
+
+}
+
+function createBody(gl) {
+    let cubeVertices = new Float32Array([
+        0,0,0,   // 0 - DLB
+        0,0,1,    // 1 - DLF
+        1,0,1,     // 2 - DRF
+        1,0,0,    // 3 - DRF
+
+        0,1,0,   // 4 - ULB
+        0,1,1,    // 5 - ULF
         1,1,1,     // 6 - URF
-        1,1,-1,    // 7 - URF
+        1,1,0,    // 7 - URF
     ]);
 
     let cubeIndices =  new Float32Array([
